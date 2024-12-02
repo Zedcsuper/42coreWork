81d90a780b9ec0c1807e2c140b3c6787fe6908fa
Zedcsuper <zedhmd7@gmail.com>
2024-12-02 21:22:31 +0300

    commit

---
+++ fullLibft/seplib/ft_printf/ft_printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zjamaien <zjamaien@student.42amman.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/18 20:17:51 by zjamaien          #+#    #+#             */
+/*   Updated: 2024/10/05 17:30:12 by zjamaien         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+void	ft_putchar_len(char c, int *len);
+void	ft_putstr_len(char *s, int *len);
+void	ft_putnbr_len(int n, int *len);
+void	ft_hex_len(unsigned int x, int *len, char xX);
+void	ft_putunsignednbr_len(unsigned int u, int *len);
+void	ft_putptr_len(size_t ptr, int *len);
+int		ft_printf(const char *s, ...);
+
+#endif
